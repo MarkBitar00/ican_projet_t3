d127bf69553400d2141aeff28773c7ee5cdc9c88
Mark Bitar <markbitar00@gmail.com>
2023-07-09 23:06:30 +0200

    fix: hurdy gurdy

--- Assets/Puzzles/HurdyGurdy/HurdyGurdyKey.cs
+++ Assets/Puzzles/HurdyGurdy/HurdyGurdyKey.cs
@@ -1,16 +1,23 @@
+using System;
 using UnityEngine;
 
 public class HurdyGurdyKey : MonoBehaviour
 {
     [SerializeField] private HurdyGurdyManager melodyManager;
     [SerializeField] public int note;
-    
+
+    private Animator animator;
     private string[] noteSoundEvents = new string[8]{"NoteOne", "NoteTwo", "NoteThree", "NoteFour", "NoteFive", "NoteSix", "NoteSeven", "NoteEight"};
-    
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void PlayNote()
     {
         if (!melodyManager.lever.GetIsMoving() || melodyManager.GetIsPlayingResolutionMelody()) return;
-        // GetComponent<Animator>().Play($"{noteSoundEvents[note - 1]}Anim");
+        animator.SetTrigger("PlayNote");
         FMODUnity.RuntimeManager.PlayOneShot($"event:/Diegetic/Sounds/HurdyGurdy/{noteSoundEvents[note - 1]}");
         if (melodyManager.GetIsSolved()) return;
         melodyManager.AddNoteToSequence(note);
