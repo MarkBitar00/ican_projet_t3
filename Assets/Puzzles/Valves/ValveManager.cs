f7881ea423d6ec6db03b2ff34a2f542af79a337b
Mathieu Bongibault <mathjick8585@gmail.com>
2023-07-11 21:57:38 +0200

    finished valve manager, to be tested

--- Assets/Puzzles/Valves/ValveManager.cs
+++ Assets/Puzzles/Valves/ValveManager.cs
@@ -1,25 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [System.Serializable]
 public class ValveProperties
 {
-    GameObject valve;
-    float TargetRotation;
+    public GameObject valve;
+    public float TargetRotation;
 }
 public class ValveManager : MonoBehaviour
 {
     public List<ValveProperties> Valves;
-    // Start is called before the first frame update
-    void Start()
+    public float accuracy;
+    [SerializeField] private UnityEvent WhenSolved;
+    public void checkValuesOfValves()
     {
-        
+        int _valvesGood = 0;
+        foreach (var valve in Valves)
+        {
+            if(Mathf.Abs(valve.valve.transform.rotation.eulerAngles.x - valve.TargetRotation) < accuracy)
+            {
+                _valvesGood++;
+            }
+        }
+        if (_valvesGood == Valves.Count)
+        {
+            Finished();
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Finished()
     {
-        
+        WhenSolved.Invoke();
     }
 }
